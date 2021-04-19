098df946c47f8f2bc8df31067e6782abffe98182
Zenodeon <32438218+Zenodeon@users.noreply.github.com>
2021-04-19 18:01:27 +0400

    touch testt

---
+++ src/InputManager.ts
@@ -0,0 +1,24 @@
+var count = 0;
+
+var countText:HTMLElement;
+
+function BodyLoaded()
+{
+var obj = document.getElementById('touchzone');
+obj.addEventListener('touchstart', TouchHandler, false);
+obj.addEventListener('mousedown', TouchHandler, false);
+
+countText = document.getElementById('countID');
+
+UpdateCount();
+}
+
+function TouchHandler()
+{
+UpdateCount();
+}
+
+function UpdateCount()
+{
+countText.innerText = "Count : " + count++;
+}
