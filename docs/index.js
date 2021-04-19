6d2ca116008ea254b9730952596ca580d488321b
Zenodeon <32438218+Zenodeon@users.noreply.github.com>
2021-04-19 21:51:12 +0400

    you cant send udp shit on brower, SMHHH

---
+++ docs/index.js
@@ -0,0 +1,32 @@
+var tcount = 0;
+var rcount = 0;
+var tCountText;
+var rCountText;
+function BodyLoaded() {
+    SendUDPData("Test Connection Response");
+    var obj = document.getElementById('touchzone');
+    obj.addEventListener('touchstart', TouchStart, false);
+    obj.addEventListener('touchend', TouchEnd, false);
+    obj.addEventListener('mousedown', TouchStart, false);
+    obj.addEventListener('mouseup', TouchEnd, false);
+    tCountText = document.getElementById('touchCountID');
+    rCountText = document.getElementById('releaseCountID');
+    UpdateTCount();
+    UpdateRCount();
+}
+function TouchStart() {
+    UpdateTCount();
+    SendUDPData("Touching");
+}
+function TouchEnd() {
+    UpdateRCount();
+    SendUDPData("Un Touch");
+}
+function UpdateTCount() {
+    tCountText.innerText = "Count : " + tcount++;
+}
+function UpdateRCount() {
+    rCountText.innerText = "Count : " + rcount++;
+}
+function SendUDPData(data) {
+}
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,49 @@
 var tcount = 0;
 var rcount = 0;
-var tCountText;
-var rCountText;
-function BodyLoaded() {
+
+var tCountText:HTMLElement;
+var rCountText:HTMLElement;
+
+function BodyLoaded()
+{
+    SendUDPData("Test Connection Response");
+    
     var obj = document.getElementById('touchzone');
     obj.addEventListener('touchstart', TouchStart, false);
     obj.addEventListener('touchend', TouchEnd, false);
     obj.addEventListener('mousedown', TouchStart, false);
     obj.addEventListener('mouseup', TouchEnd, false);
+
     tCountText = document.getElementById('touchCountID');
     rCountText = document.getElementById('releaseCountID');
+
     UpdateTCount();
     UpdateRCount();
 }
-function TouchStart() {
+
+function TouchStart()
+{
     UpdateTCount();
+    SendUDPData("Touching");
 }
-function TouchEnd() {
+
+function TouchEnd()
+{
     UpdateRCount();
+    SendUDPData("Un Touch");
 }
-function UpdateTCount() {
+
+function UpdateTCount()
+{
     tCountText.innerText = "Count : " + tcount++;
 }
-function UpdateRCount() {
+
+function UpdateRCount()
+{
     rCountText.innerText = "Count : " + rcount++;
+}
+
+function SendUDPData(data:string)
+{
+
 }
